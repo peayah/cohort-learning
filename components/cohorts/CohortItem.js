8834589b2e4e400e84721c535f6b5a24257e2634
peayah <82358003+peayah@users.noreply.github.com>
2022-07-08 21:04:58 -0700

    add function to detail button

--- components/cohorts/CohortItem.js
+++ components/cohorts/CohortItem.js
@@ -1,7 +1,14 @@
+import { useRouter } from 'next/router';
 import Card from '../ui/Card';
 import classes from './CohortItem.module.css';
 
 function CohortItem(props) {
+
+    const router = useRouter()
+
+    function showDetailsHandler() {
+        router.push("/" + props.id);
+    }
     return (
         <li className={classes.item}>
             <Card>
@@ -14,7 +21,7 @@ function CohortItem(props) {
                     <address>{props.address}</address>
                 </div>
                 <div className={classes.actions}>
-                    <button>Show Details</button>
+                    <button onClick={showDetailsHandler}>Show Details</button>
                 </div>
             </Card>
         </li>

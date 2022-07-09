69bcb41bf1f94618dd2b77fedfa4fdf485ff7043
peayah <82358003+peayah@users.noreply.github.com>
2022-07-08 23:53:05 -0700

    set up add cohort handler

--- pages/new-cohort/index.js
+++ pages/new-cohort/index.js
@@ -1,9 +1,21 @@
 import NewCohortForm from "../../components/cohorts/NewCohortForm";
+import { useRouter} from "next/router";
 
 function NewCohortPage() {
-    function addCohortHandler(enteredCohortData) {
-        console.log(enteredCohortData)
+const router = useRouter();
+
+    async function addCohortHandler(enteredCohortData) {
+        const response  = await fetch('/api/new-meetup', { 
+            method: "POST",
+            body: JSON.stringify(enteredCohortData),
+            headers: {
+                "Content-Type":"application/json"
+            }
+        });
+        const data = await response.json();
+        console.log(data)
+        router.push("/")
     }
-        return <NewCohortForm onAddCohort={addCohortHandler}/>
-    }
+    return <NewCohortForm onAddCohort={addCohortHandler}/>
+}
 export default NewCohortPage;

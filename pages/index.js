a794f7f1ca708c19041da2d9a52dd53979dec183
peayah <82358003+peayah@users.noreply.github.com>
2022-07-08 19:29:47 -0700

    update node

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,43 @@
 import CohortList from "../components/cohorts/CohortList"
 
+const TEST_ARRAY = [
+    {
+    id: "c1",
+    title: "JAM Stack Cohort",
+    image: "https://images.unsplash.com/photo-1568992687947-868a62a9f521?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
+    day: "Monday",
+    time: "6pm",
+    address: "123 Main Street, CA 12345 Town",
+    description: "This is a JAM cohort"
+    }, {
+        id: "c2",
+        title: "JS Cohort",
+        image: "https://images.unsplash.com/photo-1568992687947-868a62a9f521?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
+        day: "Tuesday",
+        time: "6pm",
+        address: "123 Main Street, CA 12345 Town",
+        description: "This is a JS cohort"
+    }, {
+        id: "cc3",
+        title: "HTML Cohort",
+        image: "https://images.unsplash.com/photo-1568992687947-868a62a9f521?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
+        day: "Wednesday",
+        time: "6pm",
+        address: "123 Main Street, CA 12345 Town",
+        description: "This is a HTML cohort"
+    }, {
+        id: "c4",
+        title: "API Cohort",
+        image: "https://images.unsplash.com/photo-1568992687947-868a62a9f521?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80",
+        day: "Thursday",
+        time: "6pm",
+        address: "123 Main Street, CA 12345 Town",
+        description: "This is a API cohort"
+    }
+];
+
 function Homepage() {
-    return <CohortList cohorts={CohortList}
+    return <CohortList cohorts={TEST_ARRAY}/>
 }
 
 export default Homepage;
